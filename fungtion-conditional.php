3fe2261be34e24b5fc276de4273acfb7b6861d8a
Siti Maelani Saadah <sitimaelanisaadah@gmail.com>
2023-08-07 09:36:22 +0700

    upload-soal-3

--- fungtion-conditional.php
+++ fungtion-conditional.php
@@ -79,12 +79,25 @@ NB: DILARANG menggunakan built-in function PHP seperti strrev() dll. Gunakan loo
 
 
 // Code function di sini
+function palindrome($string) {
+    $length = strlen($string);
+    $palindrome = true;
 
+    for ($i = 0; $i < $length / 2; $i++) {
+        if ($string[$i] !== $string[$length - $i - 1]) {
+            $palindrome = false;
+            break;
+        }
+    }
+
+    echo $palindrome ? "true" : "false";
+    echo "<br>";
+  }
 // Hapus komentar di bawah ini untuk jalankan code
-// palindrome("civic") ; // true
-// palindrome("nababan") ; // true
-// palindrome("jambaban"); // false
-// palindrome("racecar"); // true
+  palindrome("civic") ; // true
+  palindrome("nababan") ; // true
+  palindrome("jambaban"); // false
+  palindrome("racecar"); // true
 
 
 echo "<h3>Soal No 4 Tentukan Nilai </h3>";
@@ -96,13 +109,7 @@ Selain itu jika parameter integer lebih besar sama dengan 70 dan lebih kecil dar
 sama dengan 60 dan lebih kecil dari 70 maka akan mereturn string “Cukup” selain itu maka akan mereturn string “Kurang”
 */
 
-// Code function di sini
 
-// Hapus komentar di bawah ini untuk jalankan code
-// echo tentukan_nilai(98); //Sangat Baik
-// echo tentukan_nilai(76); //Baik
-// echo tentukan_nilai(67); //Cukup
-// echo tentukan_nilai(43); //Kurang
 
 
 ?>
